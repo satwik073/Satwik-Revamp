0ffd33b1c321cdba3b278ce5f84a926d4d9bf73b
satwikWiz <satwik@wizcommerce.com>
2025-03-23 23:49:20 +0530

    feat:PROD revamping initialised ( Updated UI )

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,13 +1,10 @@
-// @ts-nocheck
 import { cn } from "@/lib/utils"
 
-function Skeleton({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      data-slot="skeleton"
+      className={cn("bg-accent animate-pulse rounded-md", className)}
       {...props}
     />
   )
